f0a14038ee59c2e4669c4c8c68d631ec901a7d4b
riteshk-007 <codeshorts007@gmail.com>
2024-03-19 10:41:18 +0530

    Add Know component to Home page

---
+++ app/components/Know.jsx
@@ -0,0 +1,15 @@
+const Know = () => {
+  return (
+    <div className="text-[#2ec8d0] dark:text-[#00c2cb] w-full mx-auto flex items-center justify-center p-3 sm:p-4 md:p-5 flex-col bg-gray-200 dark:bg-black">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center">
+        We Know The Value Of Your Trust !!
+      </h1>
+      <p className="text-xs sm:text-sm md:text-base lg:text-xl text-center mt-5 text-gray-600 dark:text-gray-300 capitalize font-semibold">
+        On Path To Become India’s Best Affordable & Trustable <br /> Stock
+        Market Institute
+      </p>
+    </div>
+  );
+};
+
+export default Know;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Know from "./components/Know";
 
 export default function Home() {
   return (
     <div className="w-full flex-col">
       <Hero />
+      <Know />
     </div>
   );
 }

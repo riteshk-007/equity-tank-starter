905d7b9ff955f1f331387d72ba068bf068047a08
riteshk-007 <codeshorts007@gmail.com>
2024-03-29 13:23:56 +0530

    Remove unused services and add Providing component

---
+++ app/components/Providing.jsx
@@ -0,0 +1,65 @@
+import { IoPricetagsOutline } from "react-icons/io5";
+import { GrSystem } from "react-icons/gr";
+import { BiRevision, BiCertification } from "react-icons/bi";
+import { FaChartLine } from "react-icons/fa";
+import { PiTimerBold } from "react-icons/pi";
+
+const Providing = () => {
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-2 md:gap-4  bg-white dark:bg-black">
+      <p className="text-sm sm:text-md md:text-lg lg:text-xl font-semibold text-center text-[#03989e] dark:text-[#00c2cb] p-3 sm:p-4 md:p-5 flex gap-2">
+        We Are Providing Various Additional Services To Our Students
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-3 sm:gap-4 md:gap-5 p-3 sm:p-4 md:p-5 ">
+        {Services.map((service) => (
+          <div
+            key={service.id}
+            className="flex items-center justify-start gap-2 p-2 sm:p-3 md:p-4  rounded-lg"
+          >
+            <span className="text-white rounded dark:text-white p-3 text-sm sm:text-md md:text-lg lg:text-2xl bg-[#03989e] dark:bg-[#00c2cb]">
+              {service.icon}
+            </span>
+            <span className="text-black dark:text-white text-sm sm:text-md md:text-base">
+              {service.title}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Providing;
+const Services = [
+  {
+    id: 1,
+    title: "Lifetime Mentorship Support",
+    icon: <PiTimerBold />,
+  },
+  {
+    id: 2,
+    title: "Advance Price Action Guidance ",
+    icon: <IoPricetagsOutline />,
+  },
+  {
+    id: 3,
+    title: "Trading System Guidance",
+    icon: <GrSystem />,
+  },
+  {
+    id: 4,
+    title: "Revision Classes ",
+    icon: <BiRevision />,
+  },
+  {
+    id: 5,
+    title: "Convert Yourself Into A Pro",
+    icon: <FaChartLine />,
+  },
+  {
+    id: 6,
+    title: "Equity Tank ISO Certification",
+    icon: <BiCertification />,
+  },
+];
--- app/components/WhyChoose.jsx
+++ app/components/WhyChoose.jsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
-import { PiTimerBold, PiStudentBold } from "react-icons/pi";
-import { IoPricetagsOutline } from "react-icons/io5";
-import { GrSystem } from "react-icons/gr";
-import { BiRevision, BiCertification } from "react-icons/bi";
-import { FaChartLine } from "react-icons/fa";
+import { PiStudentBold } from "react-icons/pi";
 import { FcGoogle } from "react-icons/fc";
 import { IoIosStar } from "react-icons/io";
 import { LiaStarHalf } from "react-icons/lia";
 import { MdGroups } from "react-icons/md";
 import { GiTeacher } from "react-icons/gi";
+import Providing from "./Providing";
 
 const WhyChoose = () => {
   return (
@@ -37,27 +34,7 @@ const WhyChoose = () => {
           </p>
         </span>
       </div>
-      <div className="w-full flex flex-col items-center justify-center gap-2 md:gap-4  bg-white dark:bg-black">
-        <p className="text-sm sm:text-md md:text-lg lg:text-xl font-semibold text-center text-[#03989e] dark:text-[#00c2cb] p-3 sm:p-4 md:p-5 flex gap-2">
-          We Are Providing Various Additional Services To Our Students
-        </p>
-
-        <div className="grid grid-cols-1 md:grid-cols-2  gap-3 sm:gap-4 md:gap-5 p-3 sm:p-4 md:p-5 ">
-          {Services.map((service) => (
-            <div
-              key={service.id}
-              className="flex items-center justify-start gap-2 p-2 sm:p-3 md:p-4  rounded-lg"
-            >
-              <span className="text-white rounded dark:text-white p-3 text-sm sm:text-md md:text-lg lg:text-2xl bg-[#03989e] dark:bg-[#00c2cb]">
-                {service.icon}
-              </span>
-              <span className="text-black dark:text-white text-sm sm:text-md md:text-base">
-                {service.title}
-              </span>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Providing />
       <div className=" mt-5 w-full p-5">
         <div className="pb-20">
           <div className="mx-auto  h-96">
@@ -130,36 +107,3 @@ const WhyChoose = () => {
 };
 
 export default WhyChoose;
-
-const Services = [
-  {
-    id: 1,
-    title: "Lifetime Mentorship Support",
-    icon: <PiTimerBold />,
-  },
-  {
-    id: 2,
-    title: "Advance Price Action Guidance ",
-    icon: <IoPricetagsOutline />,
-  },
-  {
-    id: 3,
-    title: "Trading System Guidance",
-    icon: <GrSystem />,
-  },
-  {
-    id: 4,
-    title: "Revision Classes ",
-    icon: <BiRevision />,
-  },
-  {
-    id: 5,
-    title: "Convert Yourself Into A Pro",
-    icon: <FaChartLine />,
-  },
-  {
-    id: 6,
-    title: "Equity Tank ISO Certification",
-    icon: <BiCertification />,
-  },
-];

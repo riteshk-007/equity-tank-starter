e87802bd2be779e574cef6b52118fd2704427a78
riteshk-007 <codeshorts007@gmail.com>
2024-04-02 15:50:12 +0530

    Refactor About page layout and content

--- app/(pages)/about/page.js
+++ app/(pages)/about/page.js
@@ -8,88 +8,64 @@ import {
 
 const About = () => {
   return (
-    <div className="flex w-full flex-col">
-      <Image
-        width={1920}
-        height={1080}
-        src="/banner/4.png"
-        alt="Step 3"
-        className="h-full w-full object-cover p-5 rounded-3xl"
-      />
-      <div className=" my-1 md:my-5">
-        <h2 className="lg:text-6xl md:text-5xl text-2xl font-black text-center leading-10 text-[#03989e] dark:text-[#00c2cb] ">
-          More About Us
-        </h2>
-      </div>
-      <div className="my-1 md:my-5">
-        <h2 className="lg:text-2xl md:text-xl text-lg font-black text-center leading-10 text-gray-800 dark:text-gray-400 ">
+    <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+      <div className="lg:w-10/12 w-full">
+        <p className="font-normal text-sm leading-3 text-[#03989e] dark:text-[#00c2cb] cursor-pointer">
+          About Us
+        </p>
+        <h2 className="xl:w-8/12 lg:w-10/12 w-full font-bold text-gray-800 dark:text-gray-300 lg:text-4xl text-3xl lg:leading-10 leading-9 mt-2">
           On Mission Of Making
         </h2>
-      </div>
-      <div className="flex flex-col md:flex-row w-full items-center justify-center p-5 md:p-10">
-        <span className="text-xs lg:text-sm  mx-2  text-gray-700 dark:text-gray-300 text-start w-full md:w-1/2  md:my-3 flex flex-col">
-          <span className="my-2 capitalize text-xl  font-semibold flex items-start text-[#03989e] dark:text-[#00c2cb]">
-            Equity Tank{" "}
-            <p className="font-light text-gray-700 dark:text-gray-300">
-              {" "}
-              – Stock Market Institute In Delhi Since 2021
-            </p>
-          </span>
-          <p className="my-2 text-xl font-medium leading-relaxed">
-            Welcome, to the Leading Best Stock Market Institute In Delhi Since
-            2021.
+        <div className="flex items-center gap-2 flex-col text-center md:flex-row  w-full mt-2">
+          <h2 className="font-bold text-[#03989e] dark:text-[#00c2cb] lg:text-4xl text-3xl lg:leading-10 leading-9">
+            Equity Tank
+          </h2>
+          <p className="font-light text-gray-800 dark:text-gray-300 lg:text-3xl text-2xl lg:leading-10 leading-9">
+            Stock Market Institute In Delhi Since 2021
           </p>
-          <p className="my-2 text-base">
+        </div>
+        <p className="font-normal text-lg leading-6 text-gray-600 dark:text-gray-300  mt-6">
+          Welcome, to the Leading Best Stock Market Institute In Delhi Since
+          2021.
+        </p>
+      </div>
+
+      <div className="lg:mt-14 sm:mt-10 mt-12">
+        <Image
+          width={500}
+          height={300}
+          className="block w-full rounded-lg"
+          src="/banner/4.png"
+          alt="about us image"
+        />
+      </div>
+
+      <div className="lg:mt-16 sm:mt-12 mt-16 flex lg:flex-row justify-between flex-col lg:gap-8 gap-12">
+        <div className="w-full xl:w-5/12 lg:w-6/12">
+          <h2 className="font-bold lg:text-4xl text-3xl lg:leading-9 leading-7 text-gray-800 dark:text-gray-100">
+            On Mission Of Making
+          </h2>
+          <p className="font-normal text-base leading-6 text-gray-600 dark:text-gray-300 mt-4">
             We Started our organization in 2021, since that time we are working
             on providing best knowledge about stock market and chart trading in
             very affordable cost. It is an initiative for our new generation who
             have interest on making more money, Willing to do something new, and
-            want to be achieve FINANCIAL FREEDOM. Usually, we believe that
-            trading is as a business that is not made for everyone, but this is
-            just a myth that we clear by making you learn about the market &
-            strategies that gives you mind blowing results.
+            want to be achieve FINANCIAL FREEDOM.
+            <br />
+            Usually, we believe that trading is as a business that is not made
+            for everyone, but this is just a myth that we clear by making you
+            learn about the market & strategies that gives you mind blowing
+            results.
           </p>
-          <p className="my-2 text-base">
+          <p className="font-normal text-base leading-6 text-gray-600 dark:text-gray-300 mt-6">
             We are more passionate about Stock Market Trading Techniques; Short
             term trading techniques that attract wonderful earning in stock
             market.
           </p>
-        </span>
-        <div className="w-full md:w-1/2 flex items-center justify-center bg-cover">
-          <Image
-            width={600}
-            height={500}
-            src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Equity Tank"
-            className="rounded-lg object-cover"
-          />
-        </div>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl uppercase font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-[#00c2cb] to-green-600 dark:from-[#1a8085] dark:to-green-400 mb-8">
-          Our Mission Is To Make People Financially Strong And Independent
-        </h1>
-
-        <div className=" w-full text-center ">
-          <h2 className="  text-3xl sm:text-4xl md:text-5xl font-bold px-6 py-4 text-[#03989e] dark:text-[#00c2cb] ">
-            EDUCATION FOR EVERYONE
+          <h2 className="font-bold my-2 text-xl lg:leading-9 leading-7 text-gray-800 dark:text-gray-100">
+            Our Mission Is To Make People Financially Strong And Independent
           </h2>
-          <h2 className="text-xl sm:text-2xl md:text-3xl font-bold px-6 py-1 md:py-4">
-            Our Values
-          </h2>
-          <p className="text-sm sm:text-md md:text-lg lg:text-xl font-semibold text-center text-gray-900 dark:text-gray-300 px-6 py-4">
-            Utilizing better learning options can yield faster results than
-            traditional methods.
-            <br />
-            Enjoy the beauty of learning!
-          </p>
-        </div>
-
-        <div className="flex items-center flex-col md:flex-row justify-center my-8 space-x-4">
-          <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-red-500">
-            OUR SOCIAL MEDIA HANDLING
-          </h2>
-          <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-4 text-white my-3">
             <a href="#" target="_blank" rel="noopener noreferrer">
               <FaSquareFacebook
                 fontSize={35}
@@ -116,6 +92,29 @@ const About = () => {
             </a>
           </div>
         </div>
+        <div className="lg:flex items-center w-full lg:w-1/2 ">
+          <Image
+            width={400}
+            height={300}
+            className="block rounded-md  w-full"
+            src="https://images.unsplash.com/photo-1710170601257-242514895755?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="about us image"
+          />
+        </div>
+      </div>
+      <div className=" w-full text-center ">
+        <h2 className="  text-3xl sm:text-4xl md:text-5xl font-bold px-6 py-4 text-[#03989e] dark:text-[#00c2cb] ">
+          EDUCATION FOR EVERYONE
+        </h2>
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold px-6 py-1 md:py-4">
+          Our Values
+        </h2>
+        <p className="text-sm sm:text-md md:text-lg lg:text-xl font-semibold text-center text-gray-900 dark:text-gray-300 px-6 py-4">
+          Utilizing better learning options can yield faster results than
+          traditional methods.
+          <br />
+          Enjoy the beauty of learning!
+        </p>
       </div>
     </div>
   );

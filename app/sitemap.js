02537a79833360562a1172c98db454ca5cc010c7
riteshk-007 <codeshorts007@gmail.com>
2024-04-05 17:47:32 +0530

    Refactor code to improve performance and readability

---
+++ app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://equitytank.com/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://equitytank.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://equitytank.com/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://equitytank.com/offline-classes",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://equitytank.com/online-classes",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://equitytank.com/faq",
+      lastModified: new Date(),
+    },
+  ];
+}
